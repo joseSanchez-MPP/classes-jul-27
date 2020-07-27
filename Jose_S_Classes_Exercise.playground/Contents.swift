4c215c37b09300b59b88d09086de9a859d2d5de7
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-27 14:00:49 -0500

    finished exercise

---
+++ Jose_S_Classes_Exercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Car {
+    enum engine {
+        case V4, V8, V6
+    }
+    let bodyColor: String = "Red"
+    let interiorColor: String = "Blue"
+    let betterStereo: Bool = true
+}
+
+let myCar = Car();
+
