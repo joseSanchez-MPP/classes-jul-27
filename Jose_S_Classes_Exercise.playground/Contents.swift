a14d2b130cab632b6c706b7de8898dafd406666f
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-27 14:11:33 -0500

    Finished, I hope

--- Jose_S_Classes_Exercise.playground/Contents.swift
+++ Jose_S_Classes_Exercise.playground/Contents.swift
@@ -1,9 +1,10 @@
 import UIKit
 
 class Car {
-    enum engine {
-        case V4, V8, V6
-    }
+//    enum engine {
+//        case V4, V8, V6
+//    }
+    let engine = "V4"
     let bodyColor: String = "Red"
     let interiorColor: String = "Blue"
     let betterStereo: Bool = true
